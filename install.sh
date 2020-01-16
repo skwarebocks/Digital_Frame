57b7ff51f82fc3e6da285bb79997ab54f068a861
skwarebocks <59812842+skwarebocks@users.noreply.github.com>
2020-01-16 13:33:05 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,34 +11,29 @@ sudo update-rc.d dnsmasq disable
 sudo update-rc.d hostapd disable
 sudo systemctl unmask hostapd.service
 
-#install and configure PHP and apache2
+#install and configure apache2
 sudo apt install apache2 -y
 sudo tee -a /etc/apache2/apache2.conf >> /dev/null <<EOT
 <Directory "/usr/lib/cgi-bin">
     AllowOverride None
     Options None
-    Require all granted
-	Options +ExecCGI
+    Require all granted	
+    Options +ExecCGI
     AddHandler cgi-script .cgi .pl .py
 </Directory>
 EOT
+
+#install PHP
 sudo apt install php libapache2-mod-php -y
 
 #install feh and dependencies (image software)
-wget https://github.com/NautiluX/slide/releases/download/v0.9.0/slide_pi_stretch_0.9.0.tar.gz
-tar xf slide_pi_stretch_0.9.0.tar.gz				
-sudo mv slide_0.9.0/slide /usr/local/bin/
-sudo apt install libexif12 qt5-default -y
-mkdir -p /home/pi/.config/lxsession/LXDE/
-sudo  tee -a /home/pi/.config/lxsession/LXDE/autostart > /dev/null <<EOT
+sudo apt-get install feh -y
+sudo  tee -a /home/pi/.xession > /dev/null <<EOT
 xset s noblank
 xset s off
 xset -dpms
-slide -p -t 60 -o 200 -p /var/wwww/html/photos
+feh -F -Z -z -D 120 --auto-rotate /var/wwww/html/photos
 EOT
-
-#install fbi
-sudo apt-get install fbi -y
 
 #install git
 sudo apt-get install git -y
@@ -56,5 +51,9 @@ sudo cp -r /home/pi/Digital_Frame/html /var/www
 sudo rm /var/www/html/index.html
 
 ifconfig
+echo "**********************************************************"
+echo "**********************************************************"
 echo "Make note of wifi mac address above and update file: /etc/dnsmasq.conf"
 echo "Reboot when finished."
+echo "**********************************************************"
+echo "**********************************************************"
